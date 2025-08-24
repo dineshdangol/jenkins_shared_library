19c63c5e8b14471046da87551979835785d737f5
dineshdangol <dines.dangol@gmail.com>
2025-08-24 19:41:03 +0000

    deploy.groovy updated

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,7 @@
 def call(String ProjectName, String Port, String Tag){
 	withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'dockerHubUser', passwordVariable: 'dockerHubPass')]){
                     sh """
-                        echo 'Remove previously existing same container"
+                        echo "Remove previously existing same container"
                         docker rm -f "${ProjectName}" 2>/dev/null || true
                         docker run -d --name "${ProjectName}" -p "0.0.0.0:${Port}:${Port}" "$dockerHubUser/${ProjectName}:${Tag}"
                         """
